7007b6c3ae48edb9b45586b28a6ae320ed95b941
Cam Roe <croe10@ford.com>
2021-02-12 13:05:30 -0500

    Lots of unknown changes from a long time ago that I didn't check in. This repository is going to be deprecated in favour of ad-base

--- src/main/java/com/cmr/faa/App.java
+++ src/main/java/com/cmr/faa/App.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
